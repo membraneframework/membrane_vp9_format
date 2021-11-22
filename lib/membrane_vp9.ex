854ba6974470b496045d789ca26c17a0ad470e5b
Maks Stachowiak <maks.stachowiak@swmansion.com>
2021-11-22 15:03:53 +0100

    add width, height

--- lib/membrane_vp9.ex
+++ lib/membrane_vp9.ex
@@ -3,5 +3,14 @@ defmodule Membrane.VP9 do
   This module implements struct describing a VP9 encoded video stream.
   """
 
-  defstruct []
+  @type width_t :: integer
+  @type height_t :: integer
+
+  @type t :: %__MODULE__{
+    width: width_t,
+    height: height_t,
+  }
+
+  @enforce_keys [:width, :height]
+  defstruct @enforce_keys
 end
