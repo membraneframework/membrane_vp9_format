920201b6acaee2f03c5bfd907bf474961dbb7539
noarkhh <jakub.pryc@gmail.com>
2024-06-12 18:39:29 +0200

    Add width, height and framerate fields, bump version

--- lib/membrane_vp9.ex
+++ lib/membrane_vp9.ex
@@ -1,7 +1,48 @@
 defmodule Membrane.VP9 do
   @moduledoc """
-  This module implements struct describing a VP9 encoded video stream.
+  This module provides a format for a VP8 encoded video stream.
   """
 
-  defstruct []
+  @typedoc """
+  Width of single frame in pixels.
+
+  Allowed values may be restricted by used encoding parameters, for example, when using
+  4:2:0 chroma subsampling dimensions must be divisible by 2.
+  """
+  @type width :: pos_integer()
+
+  @typedoc """
+  Height of single frame in pixels.
+
+  Allowed values may be restricted by used encoding parameters, for example, when using
+  4:2:0 chroma subsampling dimensions must be divisible by 2.
+  """
+  @type height :: pos_integer()
+
+  @typedoc """
+  Number of frames per second. To avoid using floating point numbers,
+  it is described by 2 integers number of frames per timeframe in seconds.
+
+  For example, NTSC's framerate of ~29.97 fps is represented by `{30_000, 1001}`.  If the
+  information about the framerate is not present in the stream, `nil` value should be used.
+  """
+  @type framerate :: {frames :: pos_integer(), seconds :: pos_integer()} | nil
+
+  @typedoc """
+  Format definition for a VP8 video stream.
+  """
+  @type t :: %__MODULE__{
+          width: width(),
+          height: height(),
+          framerate: framerate()
+        }
+
+  @enforce_keys [
+    :width,
+    :height
+  ]
+  defstruct @enforce_keys ++
+              [
+                framerate: nil
+              ]
 end
